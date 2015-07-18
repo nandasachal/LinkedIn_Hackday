074117a260e35264eedd51dbc3a41952d4a96fe0
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-07-18 10:49:53 -0700

    deploy gh-pages

--- public/src/js/module.js
+++
@@ -1,6 +0,0 @@
-angular.module('RDash', ['ui.router', 'ngCookies', 'ngMaterial'])
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    // .primaryPalette('pink')
-    // .accentPalette('orange');
-});;
